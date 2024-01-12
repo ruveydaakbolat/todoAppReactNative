3eb79f919a08237736411e03c6b81e3546058ff2
ruveydaakbolat <ruveydaakbolat3@gmail.com>
2024-01-12 15:11:16 +0300

    initialize project

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+function App() {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Todo App</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
